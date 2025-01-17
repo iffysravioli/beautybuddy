e81f8e9ff90689bf88862004ccbbf3bd2eba2245
danielle34 <danie3m3@gmail.com>
2025-01-17 07:36:22 -0500

    text rec page added

---
+++ beautybuddy_app/lib/recommendation_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RecommendationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recommendation Page'),
+        backgroundColor: Colors.blueAccent,
+      ),
+
+    );
+  }
+}
